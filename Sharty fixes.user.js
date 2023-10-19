e4a5c2f2bdc2cdc3ed31caa43970aeccf7f13888
Xyl <git@xyl.sh>
2023-10-19 15:55:34 +1000

    Sharty fixes: make POW captcha smaller

--- Sharty fixes.user.js
+++ Sharty fixes.user.js
@@ -11,12 +11,12 @@
 // @grant       GM_setValue
 // @grant       GM_xmlhttpRequest
 // @connect     *
-// @version     2.17.1
+// @version     2.17.2
 // @author      Xyl
 // @description Enhancements for the 'ty
 // ==/UserScript==
 
-const version = "v2.17.1";
+const version = "v2.17.2";
 console.log(`Sharty fixes ${version}`);
 
 const namespace = "ShartyFixes.";
@@ -924,6 +924,15 @@ document.head.insertAdjacentHTML("beforeend", `
 
   .comment-quotes {
     text-align: center;
+  }
+
+  .sqr-captcha {
+    min-width: unset !important;
+  }
+
+  .sqr-captcha-best-hash,
+  .sqr-captcha-description {
+    display: none;
   }
 
   ${isEnabled("truncate-long-posts") ? `
