98ee4a2ce5a7e08b14449c93fbd3ef3bf41980c9
Xyl <git@xyl.sh>
2023-10-26 16:01:37 +1000

    Sharty fixes: fix archive anchor

--- Sharty fixes.user.js
+++ Sharty fixes.user.js
@@ -11,12 +11,12 @@
 // @grant       GM_setValue
 // @grant       GM_xmlhttpRequest
 // @connect     *
-// @version     2.19
+// @version     2.19.1
 // @author      Xyl
 // @description Enhancements for the 'ty
 // ==/UserScript==
 
-const version = "v2.19";
+const version = "v2.19.1";
 console.log(`Sharty fixes ${version}`);
 
 const namespace = "ShartyFixes.";
@@ -228,7 +228,7 @@ function fixPost(post) {
   if (isOwnPost && getNumber("lastTime") < time.getTime()) {
     setValue("lastTime", time.getTime());
   }
-  let archiveLink = `https://archive.soyjak.in/${thread.getAttribute("data-board")}/thread/${thread.id.split("_")[1]}#${postNumber.textContent}`;
+  let archiveLink = `https://archive.soyjak.in/${thread.getAttribute("data-board")}/thread/${thread.id.split("_")[1]}#p${postNumber.textContent}`;
   postNumber.insertAdjacentHTML("afterend", `<a href=${archiveLink} target="_blank">[A]</a>`);
   timeElement.outerHTML = `<span datetime=${timeElement.getAttribute("datetime")}>${timeElement.innerText}</span>`;
   post.querySelector(".intro").insertAdjacentHTML("beforeend", `<span class="quote-buttons"></span>`);
@@ -339,7 +339,7 @@ function fixTime() {
 function archive404() {
   if (!document.querySelector("link[href*='404style']") || !window.location.pathname.includes("thread")) return;
 
-  let archiveLink = `https://archive.soyjak.in${window.location.pathname.replace(".html", "").replace("#q", "#")}`;
+  let archiveLink = `https://archive.soyjak.in${window.location.pathname.replace(".html", "").replace("#q", "#").replace("#", "#p")}`;
   document.querySelector("input[type=button]").insertAdjacentHTML(`afterend`,
   `<br><br><input type="button" value="Check the archive" onclick="window.location = '${archiveLink}'">`);
 }
